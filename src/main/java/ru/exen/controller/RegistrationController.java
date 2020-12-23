0f79d44b08d5b2ca4b9ea76b2232e0b198a43d99
EXENq <kirill118reg@gmail.com>
2020-12-23 10:36:38 +0400

    Spring Boot Mail: not working (fix in future)

--- src/main/java/ru/exen/controller/RegistrationController.java
+++ src/main/java/ru/exen/controller/RegistrationController.java
@@ -5,37 +5,51 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import ru.exen.model.Role;
 import ru.exen.model.User;
 import ru.exen.repository.UserRepository;
+import ru.exen.service.UserService;
 
 @Controller
 public class RegistrationController {
+	private final UserService userService;
+
 	@Autowired
-	private UserRepository userRepo;
-	
+	public RegistrationController(UserService userService) {
+		this.userService = userService;
+	}
+
 	@GetMapping("/registration")
-	public String registrtion() {
+	public String registration() {
 		return "registration";
 	}
 	
 	@PostMapping("/registration")
 	public String addUser(User user, Map<String, Object> model) {
-		User userFromDb = userRepo.findByUsername(user.getUsername());
-		
-		if(userFromDb != null) {
+		if(!userService.addUser(user)) {
 			model.put("message", "User already exists!");
 			return "registration";
 		}
 		
-		user.setActive(true);
-		user.setRoles(Collections.singleton(Role.USER));
-		userRepo.save(user);
-		
 		return "redirect:/login";
+	}
+
+	@GetMapping("/activate/{code}")
+	public String activate(Model model, @PathVariable String code){
+		boolean isActivated = userService.activateUser(code);
+
+		if (isActivated){
+			model.addAttribute("message", "User successfully activated");
+		} else {
+			model.addAttribute("message", "Activation code is not found");
+		}
+
+		return "login";
 	}
 	
 }
--- src/main/java/ru/exen/service/UserService.java
+++ src/main/java/ru/exen/service/UserService.java
@@ -5,22 +5,69 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-
+import org.springframework.util.StringUtils;
+import ru.exen.model.Role;
+import ru.exen.model.User;
 import ru.exen.repository.UserRepository;
+
+import java.util.Collections;
+import java.util.UUID;
 
 @Service
 public class UserService implements UserDetailsService {
 	
 	private final UserRepository userRepo;
+	private final MailSender mailSender;
 	
 	@Autowired
-	public UserService(UserRepository userRepo) {
+	public UserService(UserRepository userRepo, MailSender mailSender) {
 		this.userRepo = userRepo;
+		this.mailSender = mailSender;
 	}
 
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 		return userRepo.findByUsername(username);
 	}
-	
+
+	public boolean addUser(User user){
+		User userFromDb = userRepo.findByUsername(user.getUsername());
+
+		if(userFromDb != null){
+			return false;
+		}
+
+		user.setActive(false);
+		user.setRoles(Collections.singleton(Role.USER));
+		user.setActivationCode(UUID.randomUUID().toString());
+
+		userRepo.save(user);
+
+		if (StringUtils.isEmpty(user.getEmail())){
+			String message = String.format(
+					"Hello, %s! \n" +
+							"Welcome! Please, visit next link for activation your account: http://localhost:8080/activate/%s",
+					user.getUsername(),
+					user.getActivationCode()
+			);
+
+			mailSender.send(user.getEmail(), "Activation code", message);
+		}
+
+		return true;
+	}
+
+	public boolean activateUser(String code) {
+		User user = userRepo.findByActivationCode(code);
+
+		if (user == null){
+			return false;
+		}
+
+		user.setActivationCode(null);
+		user.setActive(true);
+		userRepo.save(user);
+
+		return true;
+	}
 }
